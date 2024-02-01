6b31f57ebd8755921899974e109f95f654196b4b
Matt Dawley <dawley@gmail.com>
2024-01-31 17:40:25 -0700

    final

--- hash_collision.py
+++ hash_collision.py
@@ -13,8 +13,8 @@ def hash_collision(k):
 
     mask = (1 << k) - 1
 
-    #seed X and Y
-    x = random.randint(0,100)
+    #seed X (randomly) and Y
+    x = random.randint(2,100)
     y = 1
 
     while True:
@@ -25,40 +25,11 @@ def hash_collision(k):
         x_hash = hashlib.sha256(str(x).encode('utf-8')).hexdigest()
         y_hash = hashlib.sha256(str(y).encode('utf-8')).hexdigest()
 
-        # Convert hash to bytes
-        """
-        x_small = str(int(x_hash[-4:], 16)).encode('utf-8')
-        zz = int(x_hash[-4:], 16)
-        print("zz:", zz)
-        print(bin(zz & mask))
-        print("x_small:", x_small)
-        """
+        # Apply mask, convert to binary
         x_masked = bin(int(x_hash,16) & mask)
-
-        """
-        print(type(int.from_bytes(x_small, 'big')))
-        print(type(mask))
-        print(bin(int.from_bytes(x_small, 'big')))
-        print(bin(int.from_bytes(x_small, 'big') & mask))
-        print(int.from_bytes(x_small, 'big'))
-        print("Full X:", x_hash)
-        print("Small X:", x_hash[-4:])
-        print("Small X Int:", int(x_hash[-4],16))
-        print("Small X Binary:", bin(int(x_hash[-4],16)))
-        print("X_Masked:", x_masked)
-        y_small = str(int(y_hash[-4:], 16)).encode('utf-8')
-        """
         y_masked = bin(int(y_hash, 16) & mask)
-        """
-        print("Full Y:", y_hash)
-        print("Small Y:", y_hash[-4:])
-        print("Small Y Int:", int(y_hash[-4],16))
-        print("Small Y Binary:", bin(int(y_hash[-4],16)))
-        print("Y_Chunk:", y_masked)
-        """
+
         if x_masked == y_masked:
             return str(x).encode('utf-8'), str(y).encode('utf-8')
 
         y += 1
-
-#print(hash_collision(20))
